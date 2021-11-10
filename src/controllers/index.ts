62d69fc2fb29ec2110841429fef1dba5a0e61c92
ala03 <ana.slno.101@gmail.com>
2021-11-10 06:11:17 -0500

    first commit

---
+++ src/controllers/index.ts
@@ -0,0 +1,6 @@
+export * from './aeropuerto.controller';
+export * from './ping.controller';
+export * from './ruta.controller';
+export * from './usuario.controller';
+export * from './vuelo.controller';
+export * from './vuelo-ruta.controller';
